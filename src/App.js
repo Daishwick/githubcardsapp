5329946282a5565ebdaedf8d9890a80d51193856
Daishwick <87265580+Daishwick@users.noreply.github.com>
2022-06-09 12:49:55 +0200

    Github Card app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Card from "./Card";
+import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+const CardList = (props) => (
+    <div>
+        {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
     </div>
-  );
+);
+
+
+class App extends React.Component {
+    state = {
+        profiles: testData,
+    };
+    addNewProfile = (profileData) => {
+        this.setState(prevState => ({
+            profiles: [...prevState.profiles, profileData]
+        }))
+        console.log('App', profileData)
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="header">the GitHub Cards App</div>
+                <Form onSubmit={this.addNewProfile}/>
+                <CardList profiles={this.state.profiles}/>
+            </div>
+        );
+    }
 }
 
 export default App;
